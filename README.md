0cf2d912a3dbae7f6e1ddaa81362ae05e4e1ea63
Malik Benkirane <mbenkirane@internetworks.blog>
2025-12-29 19:39:05 +0100

    feat(commands): add terminal and explorer user commands
    Adds five new user commands to improve workflow efficiency:
    - SensibleTerm: Opens a horizontal split terminal with 20-line height
      for quick command execution without leaving editor
    - SensibleGitCommitAndPush: Automates git workflow by saving current
      file, committing with file as message, and pushing (requires
      .fish/functions/push.fish helper)
    - SensibleExplore: Wrapper for netrw's Explore command
    - SensibleSexplore: Wrapper for netrw's split Explore
    - SensibleVexplore: Wrapper for netrw's vertical split Explore
    
    These commands standardize common operations under the Sensible prefix
    for discoverability and provide sensible defaults (like terminal
    height). The git command integrates with fish shell functions to
    streamline commit-from-commit-message workflows.
    
    Also updates README with link to real-world configuration example from
    author's personal dotfiles repository to help users understand plugin
    usage patterns.
    
    Coming up: jujutsu integration, maybe more form snacks
    
    See also https://github.com/malikbenkirane/.nvim

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # sensible.nvim
+
+For a configuration example see [.config/nvim/lua/plugins/sensible.nvim](
+https://github.com/malikbenkirane/.nvim/blob/main/lua/plugins/sensible.nvim).
+
+You will need a [push function](
+https://github.com/malikbenkirane/.fish/blob/main/functions/push.fish)
+loaded in your shell to use `SensibleGitCommitAndPush`
--- lua/sensible/init.lua
+++ lua/sensible/init.lua
@@ -1,9 +1,34 @@
 local M = {}
 
 M.setup = function(opts)
+
   vim.api.nvim_create_user_command('SensibleCommand', function(_)
     vim.keymap.set('n', ':', 'q:i')
   end, {desc = 'Always open cmdline.'})
+
+  vim.api.nvim_create_user_command('SensibleTerm', function(_)
+    vim.cmd('sp|term')
+    vim.api.nvim_feedkeys(':res 20\ni', 'n', false)
+  end, {desc = 'Always open cmdline.'})
+
+  vim.api.nvim_create_user_command("SensibleGitCommitAndPush", function()
+    -- requires .fish/functions/push.fish
+    vim.cmd('write')
+    vim.cmd('term git commit --allow-empty --file % && push')
+  end, {})
+
+  vim.api.nvim_create_user_command("SensibleExplore", function()
+    vim.cmd('Explore')
+  end, {})
+
+  vim.api.nvim_create_user_command("SensibleSexplore", function()
+    vim.cmd('Sexplore')
+  end, {})
+
+  vim.api.nvim_create_user_command("SensibleVexplore", function()
+    vim.cmd('Vexplore')
+  end, {})
+
   local settings = {
     ["command"] = function()
       vim.cmd('SensibleCommand')
